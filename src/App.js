ba7fcae6f7564bb3cad4d68b39b266085ef835a3
tyukking34 <66588596+tyukking34@users.noreply.github.com>
2021-06-06 17:38:58 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>Let's develop management system!</h2>
+      <img src={logo} lat="logo" />
+      <h2>Let's develop manangement system!</h2>
     </div>
   );
 }
